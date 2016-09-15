21ebe3e398463bff488d84267a829cb7b78fe77e
Sadeghi, Mahmoud <sadeghim@gmail.com>
2016-09-15 13:52:03 +1000

    Fixed AtlasOfLivingAustralia/front-end-static#10

--- footer.php
+++ footer.php
@@ -11,57 +11,62 @@
         </p>
       </div>
 
-      <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2 hidden-print">
-        <h5>Explore</h5>
-        <ul class="link-list">
-          <li><a href="http://dashboard.ala.org.au/">Atlas Dashboard</a></li>
-          <li><a href="http://collections.ala.org.au/">Collections</a></li>
-          <li><a href="http://collections.ala.org.au/datasets">Datasets</a></li>
-          <li><a href="http://lists.ala.org.au/iconic-species">Browse iconic species</a></li>
-          <li><a href="http://biocache.ala.org.au/">Browse records</a></li>
-          <li><a href="http://regions.ala.org.au/">Browse your region</a></li>
-          <li><a href="http://biocache.ala.org.au/explore/your-area">Explore your area</a></li>
-          <li><a href="http://spatial.ala.org.au/">Spatial Portal</a></li>
-          <li><a href="https://phylolink.ala.org.au/">Phylolink</a></li>
-        </ul>
-      </div>
+        <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2 hidden-print">
+            <h5>Explore</h5>
+            <ul class="link-list">
+                <li><a href="http://dashboard.ala.org.au/">Atlas Dashboard</a></li>
+                <li><a href="http://collections.ala.org.au/">Collections</a></li>
+                <li><a href="http://collections.ala.org.au/datasets">Datasets</a></li>
+                <li><a href="http://lists.ala.org.au/iconic-species">Browse iconic species</a></li>
+                <li><a href="http://biocache.ala.org.au/">Browse records</a></li>
+                <li><a href="http://regions.ala.org.au/">Browse your region</a></li>
+                <li><a href="http://biocache.ala.org.au/explore/your-area">Explore your area</a></li>
+                <li><a href="http://spatial.ala.org.au/">Spatial Portal</a></li>
+                <li><a href="http://phylolink.ala.org.au/">Phylolink</a></li>
+            </ul>
+        </div>
 
-      <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2 hidden-print">
-        <h5>Contribute</h5>
-        <ul class="link-list">
-          <li><a href="https://biocollect.ala.org.au/">Join a CS project</a></li>
-          <li><a href="http://sightings.ala.org.au/">Record a sighting</a></li>
-          <li><a href="/get-involved/upload-data-sets/">Submit a data set</a></li>
-          <li><a href="http://volunteer.ala.org.au/">Digitise a record</a></li>
-          <li ><a href="/about-the-atlas/contact-us/">Contact Us</a></li>
+        <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2 hidden-print">
+            <h5>Contribute</h5>
+            <ul class="link-list">
+                <li><a href="http://biocollect.ala.org.au/">Join a CS project</a></li>
+                <li><a href="http://sightings.ala.org.au/">Record a sighting</a></li>
+                <li><a href="how-you-can/#submit-data-set">Submit a data set</a></li>
+                <li><a href="http://volunteer.ala.org.au/">Digitise a record</a></li>
+                <!-- <li ><a href="https://rawgit.com/AtlasOfLivingAustralia/front-end-static/master/ALA%20Website%20Recreation%20in%20Bootstrap%203/feedback-form-page.html">Provide Feedback</a></li> -->
+                <li ><a href="about-the-atlas/contact-us/">Contact Us</a></li>
 
-        </ul>
-      </div>
+            </ul>
+        </div>
 
-      <div class="clearfix visible-xs-block">
-        <!-- This fixes the alignment issues of the footer columns -->
-      </div>
+        <div class="clearfix visible-xs-block">
+            <!-- This fixes the alignment issues of the footer columns -->
+        </div>
 
-      <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2 hidden-print">
-        <h5>Learn</h5>
-        <ul class="link-list">
-          <li><a href="/about-the-atlas/">About the Atlas</a></li>
-          <li><a href="/education-resources/">Education Resources</a></li>
-          <li><a href="/faq/">FAQs</a></li>
-        </ul>
-      </div>
+        <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2 hidden-print">
+            <h5>Learn</h5>
+            <ul class="link-list">
+                <li><a href="about-the-atlas/">About the Atlas</a></li>
+                <li><a href="education-resources/">Education Resources</a></li>
+                <li><a href="get-involved/">Contribute to the Atlas</a></li>
+                <li><a href="how-you-can/#find-species-info">Find species info</a></li>
+                <li><a href="how-you-can/#download-data">Download data</a></li>
+                <li><a href="how-you-can/#record-sighting">Record a sighting</a></li>
+                <li><a href="how-you-can/#use-our-api">Use our API</a></li>
+            </ul>
+        </div>
 
-      <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2 hidden-print">
-        <h5>Participate</h5>
-        <ul class="link-list">
-          <li><a href="https://fieldcapture.ala.org.au/merit">MERIT</a></li>
-          <li><a href="http://root.ala.org.au/">BDRS</a></li>
-          <li><a href="http://zoatrack.org/">ZoaTrack</a></li>
-          <li><a href="http://www.soils2satellites.org.au/">Soils to satellites</a></li>
-          <li><a href="http://fish.ala.org.au/">FishMap</a></li>
-          <li><a href="http://volunteer.ala.org.au/">DigiVol</a></li>
-        </ul>
-      </div>
+        <div class="col-xs-6 col-sm-3 col-md-2 col-lg-2 hidden-print">
+            <h5>Participate</h5>
+            <ul class="link-list">
+                <li><a href="https://fieldcapture.ala.org.au/merit">MERIT</a></li>
+                <li><a href="http://root.ala.org.au/">BDRS</a></li>
+                <li><a href="http://oztrack.org/">ZoaTrack</a></li>
+                <li><a href="http://www.soils2satellites.org.au/">Soils to satellites</a></li>
+                <li><a href="http://fish.ala.org.au/">FishMap</a></li>
+                <li><a href="http://volunteer.ala.org.au/">DigiVol</a></li>
+            </ul>
+        </div>
 
     </div><!-- End row -->
 
