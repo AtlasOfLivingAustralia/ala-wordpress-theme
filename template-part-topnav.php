f194c7307c9c36cd381d77df86c34f27008ebaba
Matt Andrews <matt@mattandrews.id.au>
2017-05-09 10:52:23 +1000

    add autocomplete to main search form

--- template-part-topnav.php
+++ template-part-topnav.php
@@ -18,7 +18,7 @@
           <div class="col-xs-12 col-sm-4 col-md-6">
             <form id="global-search" class="banner" action="https://bie.ala.org.au/search" method="get" name="search-form">
               <div class="icon-addon addon-lg">
-                <input type="text" placeholder="Search the Atlas ..." class="form-control">
+                <input type="text" placeholder="Search the Atlas ..." class="form-control autocomplete">
                 <label for="email" class="glyphicon glyphicon-search" rel="tooltip" title="search"></label>
               </div>
             </form>
