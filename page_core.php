4a59a0b1066dc44eeed25bf33a63541a7ed9ec22
Matt Andrews <matt@mattandrews.id.au>
2017-05-09 16:03:58 +1000

    minor markup fix

--- page_core.php
+++ page_core.php
@@ -9,7 +9,7 @@
         <ul class="breadcrumb-list">
           <li><a href="/">Home</a></li>
           <!-- <li><a href="/"><span class="glyphicon glyphicon-menu-right"></span><?php // echo $GLOBALS['ALAcontentType'] ;?></a></li> -->
-          <li active"><span class="glyphicon glyphicon-menu-right"></span><?php the_title() ;?></li>
+          <li class="active"><span class="glyphicon glyphicon-menu-right"></span><?php the_title() ;?></li>
         </ul>
       </div>
     </div>
