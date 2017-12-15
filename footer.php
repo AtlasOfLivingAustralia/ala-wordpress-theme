3e56764399ce764b28ce51ce36be7df186acca78
Matt Andrews <matt@mattandrews.id.au>
2017-12-15 13:19:35 +1100

    support widget positioning
    Signed-off-by: Matt Andrews <matt@mattandrews.id.au>

--- footer.php
+++ footer.php
@@ -145,7 +145,7 @@
 
 <script type="text/javascript" src="https://s3.amazonaws.com/assets.freshdesk.com/widget/freshwidget.js"></script>
 <script type="text/javascript">
-  FreshWidget.init("", {"queryString": "&widgetType=popup&formTitle=ALA+support", "utf8": "✓", "widgetType": "popup", "buttonType": "text", "buttonText": "Support", "buttonColor": "white", "buttonBg": "#D5502A", "alignment": "2", "offset": "235px", "formHeight": "500px", "url": "https://support.ehelp.edu.au"} );
+  FreshWidget.init("", {"queryString": "&widgetType=popup&formTitle=ALA+support", "utf8": "✓", "widgetType": "popup", "buttonType": "text", "buttonText": "Support", "buttonColor": "white", "buttonBg": "#D5502A", "alignment": "2", "offset": "68px", "formHeight": "500px", "url": "https://support.ehelp.edu.au"} );
 </script>
 <?php wp_footer(); ?>
 </body>
